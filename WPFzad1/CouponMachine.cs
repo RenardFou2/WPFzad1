2fdbb8c0baa3fe126f86cbb93757fb06813a5f4a
Renard <harpi473@Gmail.com>
2024-03-04 15:32:44 +0100

    Dodaj pliki projektów.

---
+++ WPFzad1/CouponMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFzad1
+{
+    internal class CouponMachine
+    {
+        private List<string> coupons = new List<string>();
+        private Random random = new Random();
+
+        public void AddCoupon(string coupon)
+        {
+            coupons.Add(coupon);
+        }
+
+        public string DrawCoupon()
+        {
+            if (coupons.Count == 0)
+            {
+                throw new InvalidOperationException("Brak dostępnych kuponów.");
+            }
+
+            int index = random.Next(coupons.Count);
+            string drawnCoupon = coupons[index];
+            coupons.RemoveAt(index);
+            return drawnCoupon;
+        }
+
+        public bool AnyCouponsAvailable()
+        {
+            return coupons.Count > 0;
+        }
+
+        public List<string> ViewAllCoupons()
+        {
+            return coupons;
+        }
+    }
+}
---
+++ WPFzad1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFzad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CouponMachine machine = new CouponMachine();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            UpdateLabel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTB.Text = machine.DrawCoupon();
+            UpdateLabel();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            machine.AddCoupon(inputTB.Text);
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            displayLabel.Content = "Wszystkie kupony w maszynie:\n" + string.Join("\n", machine.ViewAllCoupons()); ;
+        }
+    }
+}
